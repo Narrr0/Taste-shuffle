f3f5edbb2845327d45deb45e6dd471a58b440f55
KangNaRae1 <051ssaa@naver.com>
2020-01-02 01:53:08 +0900

    first commit

---
+++ user/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.model import User
+
+def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            user=User.objects.create_user(username=request.POSt['username'], password=request.POSt['password1'])
+            auth.login(request.user)
+            return redirect('')
+    return render(request,'signup.html')
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(request,username=username,password=password)
+        if user is not None:
+            auth.login(request.user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'error':'ID 또는 비밀번호가 일치하지 않습니다.'})
+    else:
+        return render(request,'login.html')
